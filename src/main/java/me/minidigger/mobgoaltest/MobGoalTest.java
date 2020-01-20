d656399712664dfcd6abebcede177b306c064026
MiniDigger <admin@minidigger.me>
2020-01-20 21:34:11 +0100

    update

--- src/main/java/me/minidigger/mobgoaltest/MobGoalTest.java
+++ src/main/java/me/minidigger/mobgoaltest/MobGoalTest.java
@@ -1,7 +1,7 @@
 package me.minidigger.mobgoaltest;
 
 import com.destroystokyo.paper.ai.Goal;
-import com.destroystokyo.paper.ai.GoalSubType;
+import com.destroystokyo.paper.ai.GoalFlag;
 import com.destroystokyo.paper.ai.GoalType;
 import com.destroystokyo.paper.ai.VanillaGoal;
 
@@ -32,9 +32,8 @@ public final class MobGoalTest extends JavaPlugin {
             String behaviorGoals = zombie.getGoals().getAllGoals(GoalType.BEHAVIOR).stream().map(goal -> goal.getKey().getKey()).collect(Collectors.joining(", "));
             player.sendMessage("Behavior goals: " + behaviorGoals);
             player.sendMessage("Target goals: " + targetGoals);
-            while (zombie.getGoals().hasGoal(GoalType.TARGET, VanillaGoal.NEAREST_ATTACKABLE_TARGET)) {
-                zombie.getGoals().removeGoal(GoalType.TARGET, VanillaGoal.NEAREST_ATTACKABLE_TARGET);
-            }
+            zombie.getGoals().removeGoal(GoalType.TARGET, VanillaGoal.NEAREST_ATTACKABLE_TARGET);
+            zombie.getGoals().removeGoal(GoalType.BEHAVIOR, zombie.getGoals().getAllGoals(GoalType.BEHAVIOR).iterator().next());
             String targetGoals2 = zombie.getGoals().getAllGoals(GoalType.TARGET).stream().map(goal -> goal.getKey().getKey()).collect(Collectors.joining(", "));
             player.sendMessage("Target goals2: " + targetGoals2);
 
@@ -57,10 +56,61 @@ public final class MobGoalTest extends JavaPlugin {
                 }
 
                 @Override
-                public EnumSet<GoalSubType> getSubTypes() {
-                    return EnumSet.of(GoalSubType.LOOK);
+                public EnumSet<GoalFlag> getFlags() {
+                    return EnumSet.of(GoalFlag.LOOK);
                 }
             });
+
+            // remove test 1
+            villager.getGoals().addGoal(GoalType.TARGET, 2, new Goal() {
+
+                @Override
+                public boolean shouldActivate() {
+                    return true;
+                }
+
+                @Override
+                public void tick() {
+                    villager.playEffect(EntityEffect.FIREWORK_EXPLODE);
+                }
+
+                @Override
+                public NamespacedKey getKey() {
+                    return new NamespacedKey(MobGoalTest.this, "test");
+                }
+
+                @Override
+                public EnumSet<GoalFlag> getFlags() {
+                    return EnumSet.of(GoalFlag.LOOK);
+                }
+            });
+            villager.getGoals().removeGoal(GoalType.TARGET, new NamespacedKey(MobGoalTest.this, "test"));
+
+            // remove test 2
+            Goal goal = new Goal() {
+
+                @Override
+                public boolean shouldActivate() {
+                    return true;
+                }
+
+                @Override
+                public void tick() {
+                    villager.playEffect(EntityEffect.FIREWORK_EXPLODE);
+                }
+
+                @Override
+                public NamespacedKey getKey() {
+                    return new NamespacedKey(MobGoalTest.this, "test2");
+                }
+
+                @Override
+                public EnumSet<GoalFlag> getFlags() {
+                    return EnumSet.of(GoalFlag.LOOK);
+                }
+            };
+            villager.getGoals().addGoal(GoalType.TARGET, 3, goal);
+            villager.getGoals().removeGoal(GoalType.TARGET, goal);
             return true;
         }
         return false;
